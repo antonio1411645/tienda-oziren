18b3d31c954a15df1e443f039d3c2a601f3cb5d7
Antonio Miranda <antonio160835266@gmail.com>
2025-11-13 18:55:47 -0500

    actualizacion 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // 👈 asegura que los assets se carguen correctamente
   plugins: [
     react({
       babel: {
