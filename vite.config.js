5c799b0481e09b6c67c370f48c010cb9dd96d01a
Antonio Miranda <antonio160835266@gmail.com>
2025-11-13 19:06:16 -0500

    actualizacion 4

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/", // 👈 asegura que los assets se carguen correctamente
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: "/",
+  plugins: [react({
+    babel: { plugins: [['babel-plugin-react-compiler']] }
+  })],
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 })
